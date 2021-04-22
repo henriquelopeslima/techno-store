0cbd4068059d90d580297627fb4b63134ec5aa89
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2021-04-22 00:45:49 -0300

    produto

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        mensagem: "O vue está funcionando"
+        produtos: [],
+        produto: null
+    },
+    filters: {
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+        }
+    },
+    methods: {
+        fecthProdutos() {
+            fetch("./api/produtos.json")
+                .then(r => r.json())
+                .then(r => {
+                    this.produtos = r
+                })
+        },
+        fecthProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(r => {
+                    this.produto = r
+                })
+        },
+        abrirModal(id) {
+            this.fecthProduto(id)
+            window.scrollTo({ top: 0, behavior: "smooth", })
+        },
+        fecharModal(event) {
+            if (event.target === event.currentTarget) {
+                this.produto = false
+            }
+        }
+    },
+    created() {
+        this.fecthProdutos();
     }
 });
